249e016d4ffc609f27bd083238b6304317c13c25
suncap <suncap11@gmail.com>
2017-08-20 17:28:42 +0300

    angular - Week 2 - Assignment 2 - part 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -10,6 +10,7 @@ export const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'menu', component: MenuComponent},
   { path: 'contactus', component: ContractComponent},
+  { path: 'aboutus', component: AboutComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'dishdetail/:id', component: DishdetailComponent }
 ];
--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -7,6 +7,9 @@ export class LeaderService {
 
   constructor() { }
 
-  
+  getLeaders() : Leader[]{
+    return LEADERS;
+  }
+
 
 }
