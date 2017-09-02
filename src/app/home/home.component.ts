64d71034c5785ced02ada0ce2f8a8fdf1ac2098a
suncap <suncap11@gmail.com>
2017-09-02 15:17:49 +0300

    angular - Week 4 - e2e test

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Promotion } from '../shared/promotion';
 import { PromotionService} from '../services/promotion.service';
 import { Leader} from '../shared/leader';
 import { LeaderService } from '../services/leader.service';
-import { flyInOut,expand } from '../animations/app.animation';
+import { flyInOut, expand} from '../animations/app.animation';
 
 @Component({
   selector: 'app-home',
